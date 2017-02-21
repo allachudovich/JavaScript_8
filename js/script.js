77c81371b867253b2a744e697a132a12b9bb77f5
Alla Chudovich <allura@inbox.ru>
2017-02-21 10:40:35 +0200

    hw 15-16

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(function() {
+    function Human() {
+        this.name = 'unknown';
+        this.age = 'unknown';
+        this.gender = 'unknown';
+        this.height = 'unknown';
+        this.weight = 'unknown';
+    }
+
+    function Worker(job, salary) {
+        this.company = 'unknown';
+        this.salary = 'unknown';
+    }
+
+
+    function Student() {
+        this.study = 'GoIt';
+        this.grant = 'unknown';
+    }
+
+    Worker.prototype = new Human();
+    Student.prototype = new Human();
+
+    var Alla = new Worker();
+    Alla.name = 'Alla';
+    Alla.age = '31';
+    Alla.gender = 'female';
+    Alla.company = 'Microsoft';
+    Alla.salary = '$1000';
+    console.log(Alla);
+
+    var Oleg = new Student();
+    Oleg.name = 'Oleg';
+    Oleg.grant = '$100';
+    Oleg.watch =
+        console.log(Oleg);
+
+});
